d19a374a8a7e88d0acf8d54a3daf80dbecf36102
elkhoury20 <samer12.khoury@gmail.com>
2021-12-01 15:01:07 +0100

    stat fucntion, New Graphes Buttons

--- generateGraphFunc.py
+++ generateGraphFunc.py
@@ -4,7 +4,7 @@ import regionFunc as rf
 import genesExonsFunc as ge
 import generateStatFunc as gs
 import tkinter as tk
-from tkinter import StringVar, filedialog
+from tkinter import Label, StringVar, filedialog
 from tkinter.constants import ANCHOR, E, LEFT, NS, NSEW, RAISED, RIGHT, VERTICAL, W, Y
 import wget
 import validators
@@ -12,37 +12,124 @@ from glob import glob
 import os
 import numpy as np 
 import matplotlib.pyplot as plt
+from matplotlib.widgets import Button  
 import  gffutils
 from gffutils.create import create_db
 from pathlib import Path
 import sqlite3
 import pandas as pd
 
+
+
 def generateGraphFunc ():
+
+     grapheWindow = tk.Tk()
+     grapheWindow.geometry("580x50+700+100")
+     grapheWindow.title("Generer Des Graphes")
+
      co= sqlite3.connect(fs.dbName)
      c = co.cursor()
 
      global exonT
-     exonT= c.execute("SELECT end-start from features WHERE featuretype = 'exon'").fetchall()
+     exonT= c.execute("SELECT end-start from features WHERE featuretype = 'exon' and seqid='%s' and start >=%s and end<=%s"%(rf.chrSelected,rf.startSelected,rf.endSelected)).fetchall()
      
-     global geneT
-     geneT= c.execute("SELECT end-start from features WHERE featuretype = 'gene'").fetchall()
+     exonTInt = []
+     for exonLength in exonT :
+          exonTInt.append(exonLength[0])
+     #print(len(exonTInt))    
      
-     global intronT
-     intronT= c.execute("SELECT end-start from features WHERE featuretype ='intron'").fetchall()
+     exonPositions = []
+     for position in range(1,len(exonT)+1): 
+          exonPositions.append(position)
+     #print(len(exonPositions))
      
-     plt.plot(exonT,label='exon')
-     plt.plot(geneT,label='gene')
-     plt.plot(intronT,label='intron')
+     #print(exonPositions)
 
-     plt.xlabel('Nombre')
-     plt.ylabel('Taille (acide nucleique)')
-     plt.title('Distribution de la taille')
-     plt.legend(ncol=3,loc='upper right')
-     #bbox_to_anchor=(0.5,-0.1)
-     plt.show()
      co.commit()
      c.close()
      co.close()
+     
+     def generateGraphExon ():
+
+          plt.bar(exonPositions,exonTInt,label='exon')
+          #plt.hist(geneT,label='gene')
+          #plt.hist(intronT,label='intron')
+          
+
+          plt.xlabel('Position')
+          plt.ylabel('Taille (acide nucleique)')
+          plt.title('Distribution de Taille')
+          plt.legend(ncol=3,loc='upper right')
+          #bbox_to_anchor=(0.5,-0.1)
+          plt.show()
+
+     exon_Button = tk.Button(grapheWindow,text="Graphe d'exon",command=generateGraphExon)
+     exon_Button.grid(column=0,row=0,pady=10,padx=25)
+
+
+     co= sqlite3.connect(fs.dbName)
+     c = co.cursor()
+
+     global geneT
+     geneT= c.execute("SELECT end-start from features WHERE featuretype = 'gene' and seqid='%s' and start >=%s and end<=%s"%(rf.chrSelected,rf.startSelected,rf.endSelected)).fetchall()
+
+     geneTInt = []
+     for geneLength in geneT :
+          geneTInt.append(geneLength[0])
+     
+
+     genePositions = []
+     for position in range(1,len(geneT)+1): 
+          genePositions.append(position)
+
+     co.commit()
+     c.close()
+     co.close()
+
+     def generateGraphGene():
+
+          plt.bar(genePositions,geneTInt,label='Gene')
+          plt.xlabel('Position')
+          plt.ylabel('Taille (acide nucleique)')
+          plt.title('Distribution de Taille')
+          plt.legend(ncol=3,loc='upper right')
+          plt.show()
+
+     gene_Button = tk.Button(grapheWindow,text="Graphe des Genes",command=generateGraphGene)
+     gene_Button.grid(column=1,row=0,pady=10,padx=25)
+
+     co= sqlite3.connect(fs.dbName)
+     c = co.cursor()
+
+     global intronT
+     intronT= c.execute("SELECT end-start from features WHERE featuretype ='intron' and seqid='%s' and start >=%s and end<=%s"%(rf.chrSelected,rf.startSelected,rf.endSelected)).fetchall()
+
+     intronTInt = []
+     for intronLength in intronT :
+          intronTInt.append(intronLength[0])
+     
+
+     intronPositions = []
+     for position in range(1,len(intronT)+1): 
+          intronPositions.append(position)
+
+     co.commit()
+     c.close()
+     co.close()
+
+     def generateGraphIntron():
+
+          plt.bar(intronPositions,intronTInt,label='Intron')
+          plt.xlabel('Position')
+          plt.ylabel('Taille (acide nucleique)')
+          plt.title('Distribution de Taille')
+          plt.legend(ncol=3,loc='upper right')
+          plt.show()
+
+     intron_Button = tk.Button(grapheWindow,text="Graphe des Introns ",command=generateGraphIntron)
+     intron_Button.grid(column=2,row=0,pady=10,padx=25)
+
      return
+
+
 
--- generateStatFunc.py
+++ generateStatFunc.py
@@ -5,7 +5,7 @@ import genesExonsFunc as ge
 import generateGraphFunc as gg
 import tkinter as tk
 from tkinter import StringVar, filedialog
-from tkinter.constants import ANCHOR, E, LEFT, NS, NSEW, RAISED, RIGHT, VERTICAL, W, Y
+from tkinter.constants import ANCHOR, E, FALSE, LEFT, NS, NSEW, RAISED, RIGHT, VERTICAL, W, Y
 import wget
 import validators
 from glob import glob
@@ -17,8 +17,129 @@ from gffutils.create import create_db
 from pathlib import Path
 import sqlite3
 import pandas as pd
+#import statistics
+import pymysql
+import scipy as sp 
+from scipy.stats import norm
 
 def generateStatFunc ():
      statWindow = tk.Tk()
-     statWindow.geometry("500x500+700+100")
+     statWindow.geometry("780x200+700+100")
      statWindow.title("GFF Stats")
+     co= sqlite3.connect(fs.dbName)
+     c = co.cursor()
+
+     global exonAll
+     exonAll = c.execute("SELECT end-start from features WHERE featuretype = 'exon'and seqid='%s' and start >=%s and end<=%s"%(rf.chrSelected,rf.startSelected,rf.endSelected)).fetchall()
+     
+     exonAllArray = np.array(exonAll)
+
+     exonAllMean = round(exonAllArray.mean(),2)
+     exonAllMean = str(exonAllMean)
+
+     exonMin = exonAllArray.min()
+     exonMin= str(exonMin)
+
+     exonMax = exonAllArray.max()
+     exonMax= str(exonMax)
+
+     co.commit()
+     c.close()
+     co.close()
+
+
+     def resultExon() :
+
+          Result = tk.Label(statWindow,text= " Taille Moyenne Des Exons : " +exonAllMean)
+          Result.grid(column=1,row=1)
+     
+          mini_Result = tk.Label(statWindow,text= "Taille Minimale Des Exons : " +exonMin)
+          mini_Result.grid(column=1,row=2)   
+
+          max_Result = tk.Label(statWindow,text= "Taille Maximale des Exons : " +exonMax)
+          max_Result.grid(column=1,row=3) 
+
+     exon_Button = tk.Button(statWindow,text="Stat exon",command=resultExon)
+     exon_Button.grid(column=1, row=0, padx=80, pady= 10)
+     #padx=10,pady=10,side=LEFT
+
+
+     co= sqlite3.connect(fs.dbName)
+     c = co.cursor()
+
+     global geneAll 
+     geneAll = c.execute("SELECT end-start from features WHERE featuretype = 'gene' and seqid='%s' and start >=%s and end<=%s"%(rf.chrSelected,rf.startSelected,rf.endSelected)).fetchall()
+     
+     geneAllArray = np.array(geneAll)
+
+     geneAllMean = round(geneAllArray.mean(),2)
+     geneAllMean = str(geneAllMean)
+
+     geneMin = geneAllArray.min()
+     geneMin = str(geneMin)
+
+     geneMax = geneAllArray.max()
+     geneMax = str(geneMax)
+
+     co.commit()
+     c.close()
+     co.close()
+
+     def resultGene() :
+
+          gene_Result = tk.Label(statWindow,text= "Taille Moyenne Des Genes : " +geneAllMean)
+          gene_Result.grid(column=0, row=1)
+     
+
+          geneMin_Result = tk.Label(statWindow,text=" Taille Minimale Des Genes : " +geneMin)
+          geneMin_Result.grid(column=0, row=2)
+
+          geneMax_Result = tk.Label(statWindow,text= "Taille Maximale Des Genes : " +geneMax)
+          geneMax_Result.grid(column=0, row=3)
+
+     gene_Button = tk.Button(statWindow,text="Stat Gene",command=resultGene)
+     gene_Button.grid(column=0, row=0,padx=80, pady= 10 )
+
+
+     co= sqlite3.connect(fs.dbName)
+     c = co.cursor()
+
+     global intronAll
+     intronAll= c.execute("SELECT end-start from features WHERE featuretype ='intron' and seqid='%s' and start >=%s and end<=%s"%(rf.chrSelected,rf.startSelected,rf.endSelected)).fetchall()
+     
+     intronAllArray = np.array(intronAll)
+
+     intronAllMean = round(intronAllArray.mean(),2)
+     intronAllMean = str(intronAllMean)
+
+     intronMin = intronAllArray.min()
+     intronMin = str(intronMin)
+
+     intronMax = intronAllArray.max()
+     intronMax = str(intronMax)
+
+     co.commit()
+     c.close()
+     co.close()
+
+     def resultIntron() : 
+          intronMean_Result = tk.Label(statWindow,text= "Taille Moyenne Des Introns : " +intronAllMean)
+          intronMean_Result.grid(column=2, row=1)
+
+     
+          intronMin_Result = tk.Label(statWindow,text= "Taille Minimale Des Introns : " +intronMin)
+          intronMin_Result.grid(column=2, row=2)
+
+          intronMax_Result = tk.Label(statWindow,text= "Taille Maximale Des Introns : " +intronMax)
+          intronMax_Result.grid(column=2, row=3)
+
+     intron_Button = tk.Button(statWindow,text="Stat Intron",command=resultIntron)
+     intron_Button.grid(column=2, row=0, padx=80, pady= 10)
+
+     return    
+
+
+#taille totale du chromosome 
+#diagramme des % d'exon,intron,genes
+
+     
