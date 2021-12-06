ea731876bbf4b165b55e92fed2b6fa2650c2f5d9
elkhoury20 <samer12.khoury@gmail.com>
2021-12-06 16:27:16 +0100

    pas de sous fonctions

--- generateGraphFunc.py
+++ generateGraphFunc.py
@@ -22,160 +22,141 @@ import tkinter.ttk as ttk
 import ttkthemes as themes
 
 
+def generateGraphExon ():
+
+    plt.bar(exonPositions,exonTInt,label='exon')
+          #plt.hist(geneT,label='gene')
+          #plt.hist(intronT,label='intron')
+    plt.xlabel('Position')
+    plt.ylabel('Taille (acide nucleique)')
+    plt.title('Distribution de Taille')
+    plt.legend(ncol=3,loc='upper right')
+          #bbox_to_anchor=(0.5,-0.1)
+    plt.show()
+
+
+def generateGraphGene():
+
+    plt.bar(genePositions,geneTInt,label='Gene')
+    plt.xlabel('Position')
+    plt.ylabel('Taille (acide nucleique)')
+    plt.title('Distribution de Taille')
+    plt.legend(ncol=3,loc='upper right')
+    plt.show()
+
+
+def generateGraphIntron():
+
+    plt.bar(intronPositions,intronTInt,label='Intron')
+    plt.xlabel('Position')
+    plt.ylabel('Taille (acide nucleique)')
+    plt.title('Distribution de Taille')
+    plt.legend(ncol=3,loc='upper right')
+    plt.show()
+
+
+
+def generateGraphInter():
+
+    plt.bar(interPositions,interList,label='inter')
+    plt.xlabel('Position')
+    plt.ylabel('Taille (acide nucleique)')
+    plt.title('Distribution de Taille')
+    plt.legend(ncol=4,loc='upper right')
+    plt.show()
+
 
 def generateGraphFunc (window,resultsFrame):
 
-     for widget in resultsFrame.winfo_children() :
-          widget.destroy()
+    for widget in resultsFrame.winfo_children() :
+        widget.destroy()
      
-     window.geometry("730x350+350+0")
+    window.geometry("730x350+350+0")
 
      # resultsFrame = themes.ThemedTk(theme="radiance")
      # resultsFrame.geometry("835x55+300+300")
      # resultsFrame.title("Generer Des Graphes")
      # resultsFrame.configure(bg="#F6F6F5")
 
-     co= sqlite3.connect(fs.dbName)
-     c = co.cursor()
+    co= sqlite3.connect(fs.dbName)
+    c = co.cursor()
 
-     global exonT
-     exonT= c.execute("SELECT end-start from features WHERE featuretype = 'exon' and seqid='%s' and start >=%s and end<=%s"%(rf.chrSelected,rf.startSelected,rf.endSelected)).fetchall()
+    global exonT
+    exonT= c.execute("SELECT end-start from features WHERE featuretype = 'exon' and seqid='%s' and start >=%s and end<=%s"%(rf.chrSelected,rf.startSelected,rf.endSelected)).fetchall()
      
-     exonTInt = []
-     for exonLength in exonT :
-          exonTInt.append(exonLength[0])
+    global exonTInt
+    exonTInt = []
+    for exonLength in exonT :
+        exonTInt.append(exonLength[0])
      #print(len(exonTInt))    
-     
-     exonPositions = []
-     for position in range(1,len(exonT)+1): 
-          exonPositions.append(position)
+
+    global exonPositions 
+    exonPositions = []
+    for position in range(1,len(exonT)+1): 
+        exonPositions.append(position)
      #print(len(exonPositions))
-     
      #print(exonPositions)
 
-     co.commit()
-     c.close()
-     co.close()
+    geneT= c.execute("SELECT end-start from features WHERE featuretype = 'gene' and seqid='%s' and start >=%s and end<=%s"%(rf.chrSelected,rf.startSelected,rf.endSelected)).fetchall()
+
+    global geneTInt
+    geneTInt = []
+    for geneLength in geneT :
+        geneTInt.append(geneLength[0])
      
-     def generateGraphExon ():
+    global genePositions
+    genePositions = []
+    for position in range(1,len(geneT)+1): 
+        genePositions.append(position)
 
-          plt.bar(exonPositions,exonTInt,label='exon')
-          #plt.hist(geneT,label='gene')
-          #plt.hist(intronT,label='intron')
-          
+    global intronT
+    intronT= c.execute("SELECT end-start from features WHERE featuretype ='intron' and seqid='%s' and start >=%s and end<=%s"%(rf.chrSelected,rf.startSelected,rf.endSelected)).fetchall()
 
-          plt.xlabel('Position')
-          plt.ylabel('Taille (acide nucleique)')
-          plt.title('Distribution de Taille')
-          plt.legend(ncol=3,loc='upper right')
-          #bbox_to_anchor=(0.5,-0.1)
-          plt.show()
-
-     exon_Button = ttk.Button(resultsFrame,text="Graphe d'exon",command=generateGraphExon)
-     exon_Button.grid(column=1,row=1,pady=10,padx=25)
-
-
-     co= sqlite3.connect(fs.dbName)
-     c = co.cursor()
-
-     global geneT
-     geneT= c.execute("SELECT end-start from features WHERE featuretype = 'gene' and seqid='%s' and start >=%s and end<=%s"%(rf.chrSelected,rf.startSelected,rf.endSelected)).fetchall()
-
-     geneTInt = []
-     for geneLength in geneT :
-          geneTInt.append(geneLength[0])
+    global intronTInt
+    intronTInt = []
+    for intronLength in intronT :
+        intronTInt.append(intronLength[0])
      
+    global intronPositions
+    intronPositions = []
+    for position in range(1,len(intronT)+1): 
+        intronPositions.append(position)
 
-     genePositions = []
-     for position in range(1,len(geneT)+1): 
-          genePositions.append(position)
+    global startGene
+    startGene = c.execute("SELECT start from features WHERE featuretype ='gene' and strand = '+' and seqid='%s' and start >=%s and end<=%s"%(rf.chrSelected,rf.startSelected,rf.endSelected)).fetchall()
 
-     co.commit()
-     c.close()
-     co.close()
+    global endGene 
+    endGene = c.execute("SELECT end from features WHERE featuretype ='gene' and strand = '+' and seqid='%s' and start >=%s and end<=%s"%(rf.chrSelected,rf.startSelected,rf.endSelected)).fetchall()
 
-     def generateGraphGene():
+    interArray = np.column_stack((startGene,endGene))
+    print(interArray)
 
-          plt.bar(genePositions,geneTInt,label='Gene')
-          plt.xlabel('Position')
-          plt.ylabel('Taille (acide nucleique)')
-          plt.title('Distribution de Taille')
-          plt.legend(ncol=3,loc='upper right')
-          plt.show()
-
-     gene_Button = ttk.Button(resultsFrame,text="Graphe des Genes",command=generateGraphGene)
-     gene_Button.grid(column=1,row=0,pady=10,padx=25)
-
-     co= sqlite3.connect(fs.dbName)
-     c = co.cursor()
-
-     global intronT
-     intronT= c.execute("SELECT end-start from features WHERE featuretype ='intron' and seqid='%s' and start >=%s and end<=%s"%(rf.chrSelected,rf.startSelected,rf.endSelected)).fetchall()
-
-     intronTInt = []
-     for intronLength in intronT :
-          intronTInt.append(intronLength[0])
+    global interList
+    interList = []
+    for inter in range (1,len(interArray)) :
+        interList.append(interArray[inter,0] - interArray[inter-1,1])
+    print(interList)
      
+    global interPositions
+    interPositions = []
+    for interStart in range(0,len(endGene)-1) : 
+        interPositions.append(endGene[interStart][0]+1)
 
-     intronPositions = []
-     for position in range(1,len(intronT)+1): 
-          intronPositions.append(position)
-
-     co.commit()
-     c.close()
-     co.close()
-
-     def generateGraphIntron():
-
-          plt.bar(intronPositions,intronTInt,label='Intron')
-          plt.xlabel('Position')
-          plt.ylabel('Taille (acide nucleique)')
-          plt.title('Distribution de Taille')
-          plt.legend(ncol=3,loc='upper right')
-          plt.show()
-
-     intron_Button = ttk.Button(resultsFrame,text="Graphe des Introns ",command=generateGraphIntron)
-     intron_Button.grid(column=2,row=0,pady=10,padx=25)
-
-     co= sqlite3.connect(fs.dbName)
-     c = co.cursor()
-
-     global startGene
-     startGene = c.execute("SELECT start from features WHERE featuretype ='gene' and strand = '+' and seqid='%s' and start >=%s and end<=%s"%(rf.chrSelected,rf.startSelected,rf.endSelected)).fetchall()
-
-     global endGene 
-     endGene = c.execute("SELECT end from features WHERE featuretype ='gene' and strand = '+' and seqid='%s' and start >=%s and end<=%s"%(rf.chrSelected,rf.startSelected,rf.endSelected)).fetchall()
-
-     interArray = np.column_stack((startGene,endGene))
-     print(interArray)
-
-     interList = []
-     for inter in range (1,len(interArray)) :
-          interList.append(interArray[inter,0] - interArray[inter-1,1])
-     print(interList)
-     
-     interPositions = []
-     for interStart in range(0,len(endGene)-1) : 
-          interPositions.append(endGene[interStart][0]+1)
-
-     print(interPositions)
-
-     co.commit()
-     c.close()
-     co.close()
-
-     def generateGraphInter():
-
-          plt.bar(interPositions,interList,label='inter')
-          plt.xlabel('Position')
-          plt.ylabel('Taille (acide nucleique)')
-          plt.title('Distribution de Taille')
-          plt.legend(ncol=4,loc='upper right')
-          plt.show()
-
-     inter_Button = ttk.Button(resultsFrame,text="Graphe des inter ",command=generateGraphInter)
-     inter_Button.grid(column=2,row=1,pady=10,padx=25)
-     
+    print(interPositions)
 
 
+    co.commit()
+    c.close()
+    co.close()
 
+    exon_Button = ttk.Button(resultsFrame,text="Graphe d'exon",command=generateGraphExon)
+    exon_Button.grid(column=1,row=1,pady=10,padx=25)
+
+    gene_Button = ttk.Button(resultsFrame,text="Graphe des Genes",command=generateGraphGene)
+    gene_Button.grid(column=1,row=0,pady=10,padx=25)
+
+    intron_Button = ttk.Button(resultsFrame,text="Graphe des Introns ",command=generateGraphIntron)
+    intron_Button.grid(column=2,row=0,pady=10,padx=25)
+
+    inter_Button = ttk.Button(resultsFrame,text="Graphe des inter ",command=generateGraphInter)
+    inter_Button.grid(column=2,row=1,pady=10,padx=25)
--- generateStatFunc.py
+++ generateStatFunc.py
@@ -25,57 +25,20 @@ from scipy.stats import norm
 import tkinter.ttk as ttk 
 import ttkthemes as themes
 
-def generateStatFunc(window,resultsFrame):
-
-     for widget in resultsFrame.winfo_children() :
-          widget.destroy()
-     
-     window.geometry("730x380+350+0")
-
-     meanResult = ttk.Label(resultsFrame,text="")
-     meanResult.grid(column=1,row=1,pady=10,columnspan=3)
-
-     maxResult = ttk.Label(resultsFrame,text="")
-     maxResult.grid(column=1,row=2,pady=10,columnspan=3)
-
-     minResult = ttk.Label(resultsFrame,text="")
-     minResult.grid(column=1,row=3,pady=10,columnspan=3)
 
 
-
-     co= sqlite3.connect(fs.dbName)
-     c = co.cursor()
-
-     global geneAll 
-     geneAll = c.execute("SELECT end-start from features WHERE featuretype = 'gene' and seqid='%s' and start >=%s and end<=%s"%(rf.chrSelected,rf.startSelected,rf.endSelected)).fetchall()
-     
-     geneAllArray = np.array(geneAll)
-
-     geneAllMean = round(geneAllArray.mean(),2)
-     geneAllMean = str(geneAllMean)
-
-     geneMin = geneAllArray.min()
-     geneMin = str(geneMin)
-
-     geneMax = geneAllArray.max()
-     geneMax = str(geneMax)
-
-     co.commit()
-     c.close()
-     co.close()
-
-     def resultGene() :
+def resultGene() :
 
           # gene_Result = ttk.Label(resultsFrame,text= "Taille Moyenne Des Genes : " +geneAllMean)
           # gene_Result.grid(column=0, row=1,sticky=W)
-          meanResult.pack_forget()
-          meanResult.config(text= "Taille Moyenne Des Genes : " + geneAllMean)
+     meanResult.pack_forget()
+     meanResult.config(text= "Taille Moyenne Des Genes : " + geneAllMean)
 
-          minResult.pack_forget()
-          minResult.config(text=" Taille Minimale Des Genes : " + geneMin)
+     minResult.pack_forget()
+     minResult.config(text=" Taille Minimale Des Genes : " + geneMin)
 
-          maxResult.pack_forget()
-          maxResult.config(text="Taille Maximale Des Genes : " + geneMax)
+     maxResult.pack_forget()
+     maxResult.config(text="Taille Maximale Des Genes : " + geneMax)
 
           # geneMin_Result = ttk.Label(resultsFrame,text=" Taille Minimale Des Genes : " +geneMin)
           # geneMin_Result.grid(column=0, row=2,sticky=W)
@@ -83,89 +46,40 @@ def generateStatFunc(window,resultsFrame):
           # geneMax_Result = ttk.Label(resultsFrame,text= "Taille Maximale Des Genes : " +geneMax)
           # geneMax_Result.grid(column=0, row=3,sticky=W)
 
-     gene_Button = ttk.Button(resultsFrame,text="Stat Gene",command=resultGene)
-     gene_Button.grid(column=0, row=1,padx=50,sticky=W)
 
 
 
-     co= sqlite3.connect(fs.dbName)
-     c = co.cursor()
-
-     global exonAll
-     exonAll = c.execute("SELECT end-start from features WHERE featuretype = 'exon'and seqid='%s' and start >=%s and end<=%s"%(rf.chrSelected,rf.startSelected,rf.endSelected)).fetchall()
-     
-     exonAllArray = np.array(exonAll)
-
-     exonAllMean = round(exonAllArray.mean(),2)
-     exonAllMean = str(exonAllMean)
-
-     exonMin = exonAllArray.min()
-     exonMin= str(exonMin)
-
-     exonMax = exonAllArray.max()
-     exonMax= str(exonMax)
-
-     co.commit()
-     c.close()
-     co.close()
-
-
-     def resultExon() :
+def resultExon() :
 
           # Result = ttk.Label(resultsFrame,text= " Taille Moyenne Des Exons : " +exonAllMean)
           # Result.grid(column=1,row=1,sticky=W)
-          meanResult.pack_forget()
-          meanResult.config(text= "Taille Moyenne Des Exons : " + exonAllMean)
+     meanResult.pack_forget()
+     meanResult.config(text= "Taille Moyenne Des Exons : " + exonAllMean)
 
-          minResult.pack_forget()
-          minResult.config(text=" Taille Minimale Des Exons : " + exonMin)
+     minResult.pack_forget()
+     minResult.config(text=" Taille Minimale Des Exons : " + exonMin)
 
-          maxResult.pack_forget()
-          maxResult.config(text="Taille Maximale Des Exons : " + exonMax)
+     maxResult.pack_forget()
+     maxResult.config(text="Taille Maximale Des Exons : " + exonMax)
           # mini_Result = ttk.Label(resultsFrame,text= "Taille Minimale Des Exons : " +exonMin)
           # mini_Result.grid(column=1,row=2,sticky=W)   
 
           # max_Result = ttk.Label(resultsFrame,text= "Taille Maximale des Exons : " +exonMax)
           # max_Result.grid(column=1,row=3,sticky=W) 
 
-     exon_Button = ttk.Button(resultsFrame,text="Stat exon",command=resultExon)
-     exon_Button.grid(column=0, row=2,padx=50,sticky=W)
 
-
-
-     co= sqlite3.connect(fs.dbName)
-     c = co.cursor()
-
-     global intronAll
-     intronAll= c.execute("SELECT end-start from features WHERE featuretype ='intron' and seqid='%s' and start >=%s and end<=%s"%(rf.chrSelected,rf.startSelected,rf.endSelected)).fetchall()
-     
-     intronAllArray = np.array(intronAll)
-
-     intronAllMean = round(intronAllArray.mean(),2)
-     intronAllMean = str(intronAllMean)
-
-     intronMin = intronAllArray.min()
-     intronMin = str(intronMin)
-
-     intronMax = intronAllArray.max()
-     intronMax = str(intronMax)
-
-     co.commit()
-     c.close()
-     co.close()
-
-     def resultIntron() : 
+def resultIntron() : 
           
           # intronMean_Result = ttk.Label(resultsFrame,text= "Taille Moyenne Des Introns : " +intronAllMean)
           # intronMean_Result.grid(column=2, row=1,sticky=W)
-          meanResult.pack_forget()
-          meanResult.config(text= "Taille Moyenne Des Introns : " + intronAllMean )
+     meanResult.pack_forget()
+     meanResult.config(text= "Taille Moyenne Des Introns : " + intronAllMean )
 
-          minResult.pack_forget()
-          minResult.config(text=" Taille Minimale Des Introns : " + intronMin)
+     minResult.pack_forget()
+     minResult.config(text=" Taille Minimale Des Introns : " + intronMin)
 
-          maxResult.pack_forget()
-          maxResult.config(text="Taille Maximale Des Introns : " + intronMax)
+     maxResult.pack_forget()
+     maxResult.config(text="Taille Maximale Des Introns : " + intronMax)
      
           # intronMin_Result = ttk.Label(resultsFrame,text= "Taille Minimale Des Introns : " +intronMin)
           # intronMin_Result.grid(column=2, row=2,sticky=W)
@@ -173,41 +87,119 @@ def generateStatFunc(window,resultsFrame):
           # intronMax_Result = ttk.Label(resultsFrame,text= "Taille Maximale Des Introns : " +intronMax)
           # intronMax_Result.grid(column=2, row=3,sticky=W)
 
-     intron_Button = ttk.Button(resultsFrame,text="Stat Intron",command=resultIntron)
-     intron_Button.grid(column=0, row=3,padx=50,sticky=W)
 
+
+
+def generatePiechartExonsIntrons ():
+
+     global values 
+     values = [intronPielist,exonPielist]
+          
+     global Names  
+     Names = ["Intron","Exons"]
+          
+     plt.pie(values,labels=Names,autopct="%.1f%%",wedgeprops={'edgecolor':'white', 'linewidth':2})
+     plt.title('Pourcentage Des Exons Et Introns')
+     plt.show()
+
+
+def generateStatFunc(window,resultsFrame):
+
+     for widget in resultsFrame.winfo_children() :
+          widget.destroy()
+     
+     window.geometry("730x380+350+0")
+     
      co= sqlite3.connect(fs.dbName)
      c = co.cursor()
 
+     global meanResult
+     meanResult = ttk.Label(resultsFrame,text="")
+     meanResult.grid(column=1,row=1,pady=10,columnspan=3)
+
+     global maxResult
+     maxResult = ttk.Label(resultsFrame,text="")
+     maxResult.grid(column=1,row=2,pady=10,columnspan=3)
+
+     global minResult
+     minResult = ttk.Label(resultsFrame,text="")
+     minResult.grid(column=1,row=3,pady=10,columnspan=3)
+
+     global geneAll 
+     geneAll = c.execute("SELECT end-start from features WHERE featuretype = 'gene' and seqid='%s' and start >=%s and end<=%s"%(rf.chrSelected,rf.startSelected,rf.endSelected)).fetchall()
+     
+     geneAllArray = np.array(geneAll)
+     global geneAllMean
+     geneAllMean = round(geneAllArray.mean(),2)
+     geneAllMean = str(geneAllMean)
+
+     global geneMin
+     geneMin = geneAllArray.min()
+     geneMin = str(geneMin)
+
+     global geneMax
+     geneMax = geneAllArray.max()
+     geneMax = str(geneMax)
+
+     global exonAll
+     exonAll = c.execute("SELECT end-start from features WHERE featuretype = 'exon'and seqid='%s' and start >=%s and end<=%s"%(rf.chrSelected,rf.startSelected,rf.endSelected)).fetchall()
+     
+     exonAllArray = np.array(exonAll)
+     global exonAllMean
+     exonAllMean = round(exonAllArray.mean(),2)
+     exonAllMean = str(exonAllMean)
+
+     global exonMin
+     exonMin = exonAllArray.min()
+     exonMin= str(exonMin)
+
+     global exonMax
+     exonMax = exonAllArray.max()
+     exonMax= str(exonMax)
+
+     global intronAll
+     intronAll= c.execute("SELECT end-start from features WHERE featuretype ='intron' and seqid='%s' and start >=%s and end<=%s"%(rf.chrSelected,rf.startSelected,rf.endSelected)).fetchall()
+     
+     intronAllArray = np.array(intronAll)
+
+     global intronAllMean
+     intronAllMean = round(intronAllArray.mean(),2)
+     intronAllMean = str(intronAllMean)
+
+     global intronMin
+     intronMin = intronAllArray.min()
+     intronMin = str(intronMin)
+
+     global intronMax
+     intronMax = intronAllArray.max()
+     intronMax = str(intronMax)
+
      global intronPie
      intronPie = c.execute("SELECT count(end-start) from features WHERE featuretype ='intron' and seqid='%s' and start >=%s and end<=%s"%(rf.chrSelected,rf.startSelected,rf.endSelected)).fetchall()
+     global intronPielist
      intronPielist = intronPie[0][0]
- 
 
      global exonPie 
      exonPie = c.execute("SELECT count(end-start) from features WHERE featuretype ='exon' and seqid='%s' and start >=%s and end<=%s"%(rf.chrSelected,rf.startSelected,rf.endSelected)).fetchall()
+     global exonPielist
      exonPielist = exonPie[0][0]
-  
 
      co.commit()
      c.close()
      co.close()
 
-     def generatePiechartExonsIntrons ():
+     gene_Button = ttk.Button(resultsFrame,text="Stat Gene",command=resultGene)
+     gene_Button.grid(column=0, row=1,padx=50,sticky=W)
 
-          global values 
-          values = [intronPielist,exonPielist]
-          
-          global Names  
-          Names = ["Intron","Exons"]
-          
-          plt.pie(values,labels=Names,autopct="%.1f%%",wedgeprops={'edgecolor':'white', 'linewidth':2})
-          plt.title('Pourcentage Des Exons Et Introns')
-          plt.show()
+     exon_Button = ttk.Button(resultsFrame,text="Stat exon",command=resultExon)
+     exon_Button.grid(column=0, row=2,padx=50,sticky=W)
+
+     intron_Button = ttk.Button(resultsFrame,text="Stat Intron",command=resultIntron)
+     intron_Button.grid(column=0, row=3,padx=50,sticky=W)
 
      piechart_Button = ttk.Radiobutton(resultsFrame,text='PieChart',command=generatePiechartExonsIntrons)
-     piechart_Button.grid(column=0,row=6,padx=30)           
-     
+     piechart_Button.grid(column=0,row=6,padx=30) 
+
      return    
 
      
