68ca528e0e370fea4b67feb43b214dcc58420861
Mohamed Ouertani <mohamed.ouertani.1@etu.univ-amu.fr>
2021-12-12 06:34:35 +0100

    removed unnecessary/old code and commented treeview

--- genesExonsFunc.py
+++ genesExonsFunc.py
@@ -33,23 +33,12 @@ def getPlus():
 
      numberGenesList = cur.execute("SELECT count(start) FROM features WHERE featuretype='gene' and strand = '+' and seqid = '%s' and start>=%s and end <=%s"%(rf.chrSelected,rf.startSelected,rf.endSelected)).fetchall()
      numberGenes = numberGenesList[0][0]
-     # geneResult.pack_forget()
-     # geneResult.configure(text="Gènes brin + : "+str(numberGenes))
-               
-
+  
      numberExonsList = cur.execute("SELECT count(start) FROM features WHERE featuretype='exon' and strand = '+' and seqid = '%s' and start>=%s and end <=%s"%(rf.chrSelected,rf.startSelected,rf.endSelected)).fetchall()
      numberExons = numberExonsList[0][0]
-     # exonResult.pack_forget()
-     # exonResult.configure(text="Exons brin + : "+str(numberExons))
-               
+         
      numberIntronsList = cur.execute("SELECT count(start) FROM features WHERE featuretype='intron' and strand = '+' and seqid = '%s' and start>=%s and end <=%s"%(rf.chrSelected,rf.startSelected,rf.endSelected)).fetchall()
      numberIntrons = numberIntronsList[0][0]
-     # intronResult.pack_forget()
-     # intronResult.configure(text="Introns brin + : "+str(numberIntrons))
-
-
-     # selectedStrand.pack_forget()
-     # selectedStrand.config(text="Brin +")
 
      con.commit()
      cur.close()
@@ -66,20 +55,12 @@ def getMinus():
 
      numberGenesList = cur.execute("SELECT count(start) FROM features WHERE featuretype='gene' and strand = '-' and seqid = '%s' and start>=%s and end <=%s"%(rf.chrSelected,rf.startSelected,rf.endSelected)).fetchall()
      numberGenes = numberGenesList[0][0]
-     # geneResult.pack_forget()
-     # geneResult.configure(text="Gènes brin - : "+str(numberGenes))
-          
 
      numberExonsList = cur.execute("SELECT count(start) FROM features WHERE featuretype='exon' and strand = '-' and seqid = '%s' and start>=%s and end <=%s"%(rf.chrSelected,rf.startSelected,rf.endSelected)).fetchall()
      numberExons = numberExonsList[0][0]
-     # exonResult.pack_forget()
-     # exonResult.configure(text="Exons brin - : "+str(numberExons))
 
      numberIntronsList = cur.execute("SELECT count(start) FROM features WHERE featuretype='intron' and strand = '-' and seqid = '%s' and start>=%s and end <=%s"%(rf.chrSelected,rf.startSelected,rf.endSelected)).fetchall()
      numberIntrons = numberIntronsList[0][0]
-     # intronResult.pack_forget()
-     # intronResult.configure(text="Introns brin - : "+str(numberIntrons))
-     
 
      con.commit()
      cur.close()
@@ -96,23 +77,13 @@ def getBoth():
      cur = con.cursor()
                
      numberGenesList = cur.execute("SELECT count(start) FROM features WHERE featuretype='gene' and seqid = '%s' and start>=%s and end <=%s"%(rf.chrSelected,rf.startSelected,rf.endSelected)).fetchall()
-     global numberGenes
      numberGenes = numberGenesList[0][0]
-     # geneResult.pack_forget()
-     # geneResult.configure(text="Gènes 2 brins : "+str(numberGenes))
-               
-               
+    
      numberExonsList = cur.execute("SELECT count(start) FROM features WHERE featuretype='exon' and seqid = '%s' and start>=%s and end <=%s"%(rf.chrSelected,rf.startSelected,rf.endSelected)).fetchall()
-     global numberExons
      numberExons = numberExonsList[0][0]
-     # exonResult.pack_forget()
-     # exonResult.configure(text="Exons 2 brins : "+str(numberExons))
 
      numberIntronsList = cur.execute("SELECT count(start) FROM features WHERE featuretype='intron' and seqid = '%s' and start>=%s and end <=%s"%(rf.chrSelected,rf.startSelected,rf.endSelected)).fetchall()
-     global numberIntrons
      numberIntrons = numberIntronsList[0][0]
-     # intronResult.pack_forget()
-     # intronResult.configure(text="Introns 2 brins : "+str(numberIntrons))
 
      con.commit()
      cur.close()
@@ -137,7 +108,7 @@ def genesExonsFunc(window,resultsFrame,selectedRegion):
 
           genesExonsTitle = ttk.Label(resultsFrame,text="Nombre de gènes et d'exons",foreground="black")
           genesExonsTitle.grid(column=0,row=0,pady=15,columnspan=4)
-
+          #Create treeview widget
           numbersTab = ttk.Treeview(resultsFrame,height=3)
           
 
@@ -145,50 +116,23 @@ def genesExonsFunc(window,resultsFrame,selectedRegion):
           minusGenes,minusExons,minusIntrons=getMinus()
           bothGenes,bothExons,bothIntrons=getBoth()
 
+          #Define columns
           numbersTab["columns"] = ("Genes","Exons","Introns")
-
+          #Format columns
           numbersTab.column("#0",anchor=CENTER,width=120,minwidth=120)
           numbersTab.column("Genes",anchor=CENTER,width=120,minwidth=120)
           numbersTab.column("Exons",anchor=CENTER,width=120,minwidth=120)
           numbersTab.column("Introns",anchor=CENTER,width=120,minwidth=120)
-
+          #Create headings
           numbersTab.heading("#0",text="",anchor=CENTER)
           numbersTab.heading("Genes",text="Genes",anchor=CENTER)
           numbersTab.heading("Exons",text="Exons",anchor=CENTER)
           numbersTab.heading("Introns",text="Introns",anchor=CENTER)
-
+          #Insert values
           numbersTab.insert(parent="",index='end',iid=0,text="Brin +",value=(plusGenes,plusExons,plusIntrons))
           numbersTab.insert(parent="",index='end',iid=1,text="Brin -",value=(minusGenes,minusExons,minusIntrons))
           numbersTab.insert(parent="",index='end',iid=2,text="2 Brins",value=(bothGenes,bothExons,bothIntrons))
 
           numbersTab.grid(column=0,row=1,rowspan=1,columnspan=4)
 
-
-
-
-          # global geneResult
-          # geneResult = ttk.Label(resultsFrame,text="")
-          # geneResult.grid(column=1,row=2,pady=10,columnspan=1)
-          # global exonResult
-          # exonResult = ttk.Label(resultsFrame,text="")
-          # exonResult.grid(column=1,row=3,pady=10,columnspan=1)
-          # global intronResult
-          # intronResult = ttk.Label(resultsFrame,text="")
-          # intronResult.grid(column=1,row=4,pady=10,columnspan=1)
-
-          # global plusButton
-          # plusButton = ttk.Button(resultsFrame,text="Brin +",command=getPlus)
-          # plusButton.grid(column=0,row=1,padx=40,pady=10)
-
-          # global minusButton
-          # minusButton = ttk.Button(resultsFrame,text="Brin -",command=getMinus)
-          # minusButton.grid(column=1,row=1,padx=40,pady=10)
-
-          # global bothButton
-          # bothButton = ttk.Button(resultsFrame,text="2 Brins",command=getBoth)
-          # bothButton.grid(column=2,row=1,padx=40,pady=10)
-
-
-
-         
      return
